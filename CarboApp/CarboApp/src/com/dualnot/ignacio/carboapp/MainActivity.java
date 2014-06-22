53860e31ba8796483370ce39f18a73222d7c6aba
Ignacio Rodriguez <rodriguez.fernandez.ignacio@gmail.com>
2014-06-22 20:54:34 +0200

    Action bar with two new actions: camera and search.
    Options are not shown as icons. This must be fixed.

--- CarboApp/CarboApp/src/com/dualnot/ignacio/carboapp/MainActivity.java
+++ CarboApp/CarboApp/src/com/dualnot/ignacio/carboapp/MainActivity.java
@@ -1,17 +1,16 @@
 package com.dualnot.ignacio.carboapp;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
-import android.os.Build;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 	public static final String EXTRA_MESSAGE = "com.dualnot.ignacio.MESSAGE";
@@ -27,6 +26,7 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
 
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
@@ -47,12 +47,31 @@ public class MainActivity extends ActionBarActivity {
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
+		/*int id = item.getItemId();
 		if (id == R.id.action_settings) {
 			return true;
+		}*/
+		switch (item.getItemId()) {
+			case R.id.action_search: 
+				openSearch();
+				return true;
+			case R.id.action_camera: 
+				openCamera();
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
 		}
-		return super.onOptionsItemSelected(item);
+		
 	}
+	
+	public void openSearch(){
+		Toast.makeText(getApplicationContext(), "Search!", Toast.LENGTH_SHORT).show();
+	}
+	
+	public void openCamera(){
+		Toast.makeText(getApplicationContext(), "Camera!", Toast.LENGTH_SHORT).show();
+	}
+	
 
 	/**
 	 * A placeholder fragment containing a simple view.
